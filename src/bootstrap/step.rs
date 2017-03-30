ead5fcf28d5c15a80b677fd52b8c0f1493f67dbd
Nathan Stocks <nathan.stocks@gmail.com>
2017-03-29 21:38:23 -0600

    A magic incantation to get openssl to actually (hopefully) compile, and some signposts for easier visibility of that section.

--- src/bootstrap/step.rs
+++ src/bootstrap/step.rs
@@ -491,7 +491,7 @@ pub fn build_rules<'a>(build: &'a Build) -> Rules {
 
     rules.build("test-helpers", "src/rt/rust_test_helpers.c")
          .run(move |s| native::test_helpers(build, s.target));
-    rules.build("openssl", "path/to/nowhere")
+    rules.build("openssl", "openssl")
          .run(move |s| native::openssl(build, s.target));
 
     // Some test suites are run inside emulators, and most of our test binaries
